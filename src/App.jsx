cff0b18ab391e54aa76e700ba11e6d903dcd2aaa
asad3473 <asadsoomro0311@gmail.com>
2025-02-04 11:42:42 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
 
+          <Route path='/' element={'home'} />
+          <Route path='/services' element={'home'} />
+          <Route path='/shop' element={'home'} />
+          <Route path='/contact' element={'home'} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
