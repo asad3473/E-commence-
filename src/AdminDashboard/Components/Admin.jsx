bc5a2062d7b3d2bbd1deed9a3b094b2034267be6
asad3473 <asadsoomro0311@gmail.com>
2025-02-06 14:27:41 +0500

    admin

---
+++ src/AdminDashboard/Components/Admin.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Admin() {
+  return (
+    <div>
+        
+        <div className=" flex flex-col sm:flex-row border-2 border-black">
+            <div className=" w-full border border-gray-400 sm:w-[30%]">
+                {/* sidebar */}
+                <h1>Admin</h1>
+            </div>
+            <div className="">
+                {/* product */}
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sed, aut ut necessitatibus similique voluptate suscipit eligendi autem sapiente temporibus, laborum neque nemo eaque! Saepe delectus eligendi laudantium velit tenetur.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './Components/Navbar'
-import Footter from './Components/Footter'
-import Home from './Components/Home/Home'
-import Store from './Pages/Store'
-import LogIn from './Pages/LogIn'
-import SignUp from './Pages/SignUp'
-import AllShops from './Components/Shop/AllShops'
-import Blogs from './Components/Blogs/Blogs'
-import ContactUs from './Components/Contact/ContactUs'
-import AddCart from './Pages/AddCart'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Footter from './Components/Footter';
+import Home from './Components/Home/Home';
+import Store from './Pages/Store';
+import LogIn from './Pages/LogIn';
+import SignUp from './Pages/SignUp';
+import AllShops from './Components/Shop/AllShops';
+import Blogs from './Components/Blogs/Blogs';
+import ContactUs from './Components/Contact/ContactUs';
+import AddCart from './Pages/AddCart';
+import Admin from './AdminDashboard/Components/Admin';
 
-function App() {
-
-
+function MainLayout() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-
-          <Route path='/' element={<Home />} />
-          <Route path='/blogs' element={<Blogs/>} />
-          <Route path='/store' element={<Store />} />
-          <Route path='/shop' element={<AllShops/>} />
-          <Route path='/signup' element={<SignUp/>} />
-          <Route path='/contact' element={<ContactUs/>} />
-          <Route path='/login' element={<LogIn/>} />
-          <Route path='/login' element={<LogIn/>} />
-          <Route path='/cart' element={<AddCart/>} />
-        </Routes>
-        <Footter />
-      </BrowserRouter>
+      <Navbar />
+      <Outlet />
+      <Footter />
     </>
-  )
+  );
 }
 
-export default App
+function AdminLayout() {
+  return <Outlet />;
+}
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/blogs', element: <Blogs /> },
+      { path: '/store', element: <Store /> },
+      { path: '/shop', element: <AllShops /> },
+      { path: '/contact', element: <ContactUs /> },
+      { path: '/cart', element: <AddCart /> },
+    ],
+  },
+  { path: '/signup', element: <SignUp /> },
+  { path: '/login', element: <LogIn /> },
+  {
+    element: <AdminLayout />,
+    children: [
+      { path: '/admin', element: <Admin /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
